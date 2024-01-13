9b23c389c2c0e610af9c096f57bbb37bdeabb2f2
Michael Selvesakis <miselve@cs.ihu.gr>
2024-01-13 23:37:08 +0200

    Launch.bat change

--- launch.bat
+++ launch.bat
@@ -2,6 +2,6 @@
 SET var=%cd%
 
 start cmd /c "cd %var%\diabetes-python && start run_py.bat"
-start cmd /c "cd %var%\diabetes-prediction-react && npm start"
+start cmd /c "cd %var%\diabetes-prediction-react && serve -s build"
 
 echo Application is running. Close this window to stop the servers.
